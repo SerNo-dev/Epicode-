0d3772409a15dcef08fc147745b79596b22b1c54
SerNo-Dev <intell@hotmail.it>
2024-01-29 15:15:22 +0100

    Salve assas

--- S1/E1/D1.js
+++ S1/E1/D1.js
@@ -65,7 +65,7 @@ console.log(sottrazione);
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
- EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!). xx
 */
 
 let name1 = "john";
