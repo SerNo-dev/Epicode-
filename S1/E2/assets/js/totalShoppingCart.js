b4863c925013100613c5174b9199e4803195b743
SerNo-Dev <intell@hotmail.it>
2024-01-30 16:40:00 +0100

    Esercizio 2

--- S1/E2/assets/js/totalShoppingCart.js
+++
@@ -1,6 +0,0 @@
-/* ESERCIZIO 5
-  Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
-  C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
-  Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
-*/
-let totalShoppingCart;
