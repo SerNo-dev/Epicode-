3b0cb71156db76433453a36a842bdd12b48e88e7
SerNo-Dev <intell@hotmail.it>
2024-03-07 16:35:48 +0100

    Unit 2 settimana3 Es4

--- UNIT2/S3/E4/assets/js/script.js
+++ UNIT2/S3/E4/assets/js/script.js
@@ -1,6 +1,13 @@
 const BaseUrl = 'https://api.pexels.com/v1/search?query=';
 const btnLoadImages = document.getElementById('loadImages');
 const btnSecondaryImages = document.getElementById('secondaryImages');
+const bottoniEditTohide = document.querySelectorAll('div > button:nth-child(2)');
+window.addEventListener('load',init);
+const cardBody = document.getElementsByClassName('card-body');
+let array;
+function init(){
+    editTohide()
+}
 
 btnLoadImages.addEventListener('click', () => {
     loadImages(); 
@@ -19,6 +26,8 @@ const loadImages = async () => {
         });
         let mountainsData = await mountainsResponse.json();
         displayImages(mountainsData.photos);
+        array = mountainsData;
+        console.log(array)
     } catch(error) {
         console.log(error);
     }
@@ -34,23 +43,36 @@ const displayImages = (photos) => {
 }
 
 const loadSecondaryImages = async() =>{
-   try{ let sunsetResponse = await fetch(BaseUrl + 'sunset',{
-        headers: {
-            Authorization: 'TXAl4tsq0AH939vI13rKvpyMo7xSgfkSwtTUiEnLKjjvEI0zqABAVoL5'
-        }
-    });
-    let sunsetData = await sunsetResponse.json();
-    secondaryImages(sunsetData.photos);
-    }catch(error){
-        console.log(error);
-    }
-}
-const secondaryImages = (photos) =>{
-    let immagini = document.querySelectorAll('img');
-    immagini.forEach((img,index)=>{
-        if(photos[index]){
-            img.src = photos[index].src.large
-        }
+    try{ let sunsetResponse = await fetch(BaseUrl + 'sunset',{
+         headers: {
+             Authorization: 'TXAl4tsq0AH939vI13rKvpyMo7xSgfkSwtTUiEnLKjjvEI0zqABAVoL5'
+         }
+     });
+     let sunsetData = await sunsetResponse.json();
+     secondaryImages(sunsetData.photos);
+     }catch(error){
+         console.log(error);
+     }
+ }
+ const secondaryImages = (photos) =>{
+     let immagini = document.querySelectorAll('img');
+     immagini.forEach((img,index)=>{
+         if(photos[index]){
+             img.src = photos[index].src.large
+         }
+     })
+ }
+ 
+  const editTohide = () =>{
+    bottoniEditTohide.forEach(element =>{
+        element.innerHTML = 'Hide'
     })
-}
+  }
+
+  
+bottoniEditTohide.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        deleteItem(index);
+    });
+});
 
