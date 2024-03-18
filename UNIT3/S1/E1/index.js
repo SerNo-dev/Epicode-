9ab18d1cd05edc543b92653620c7a46d48c097ce
SerNo-Dev <intell@hotmail.it>
2024-03-18 15:48:28 +0100

    E1 Unit3 S1

---
+++ UNIT3/S1/E1/index.js
@@ -0,0 +1,43 @@
+var User = /** @class */ (function () {
+    function User(_credito, _numeroChiamate, _nome, _cognome) {
+        this.credito = _credito;
+        this.numeroChiamate = _numeroChiamate;
+        this.nome = _nome;
+        this.cognome = _cognome;
+    }
+    User.prototype.ricarica = function (x) {
+        return x + this.credito;
+    };
+    User.prototype.chiamata = function (minuti) {
+        var millisecondiInUnMinuto = 60000;
+        var costoChiamataPerMinuto = 0.20;
+        if (minuti * millisecondiInUnMinuto > 0) {
+            var costoTotale = costoChiamataPerMinuto * minuti;
+            this.credito -= costoTotale;
+            console.log("Credito dopo la chiamata: ".concat(this.credito));
+        }
+    };
+    User.prototype.chiama404 = function () {
+        return "Il tuo credito \u00E8 ".concat(this.credito);
+    };
+    User.prototype.getNumeroChiamta = function () {
+        return "Minuti passi in chiamata ".concat(this.numeroChiamate);
+    };
+    return User;
+}());
+var Call = new User(20, 0, 'Pippo', 'Biachi');
+console.log(Call);
+console.log(Call.ricarica(5));
+console.log(Call.chiamata(0));
+console.log(Call.chiama404());
+console.log(Call.getNumeroChiamta());
+var credito = document.getElementById('credito');
+credito.innerText = "Il tuo credito \u00E8 ".concat(Call.credito, " euro");
+var minuti = document.getElementById('minuti');
+minuti.innerText = "".concat(Call.getNumeroChiamta());
+var ricaricaInput = document.getElementById('inputRicarica');
+var btnRicarica = document.getElementById('btnRicarica');
+btnRicarica.addEventListener('click', function () {
+    var ammontareRicarica = parseInt(ricaricaInput.value);
+    credito.innerText = "Il tuo credito \u00E8 ".concat(Call.ricarica(ammontareRicarica), " euro");
+});
---
+++ UNIT3/S1/E1/index.ts
@@ -0,0 +1,74 @@
+interface Telefono {
+    credito : number;
+    numeroChiamate: number;
+}
+
+
+class User implements Telefono{
+    credito: number;
+    numeroChiamate: number;
+    nome : string;
+    cognome: string
+    constructor (_credito: number,_numeroChiamate: number,_nome:string,_cognome : string){
+    this.credito = _credito
+    this.numeroChiamate = _numeroChiamate;
+    this.nome = _nome;
+    this.cognome = _cognome;
+    }
+
+    ricarica(x:number) {
+     return x + this.credito
+    }
+    chiamata(minuti: number):void{
+    const millisecondiInUnMinuto = 60000;
+    const costoChiamataPerMinuto = 0.20
+      if(minuti * millisecondiInUnMinuto > 0){
+        const costoTotale = costoChiamataPerMinuto * minuti;
+        this.credito -= costoTotale
+        console.log(`Credito dopo la chiamata: ${this.credito}`);
+      }
+    }
+    chiama404 () {
+        return `Il tuo credito è ${this.credito}`
+    }
+    getNumeroChiamta() {
+        return `Minuti passi in chiamata ${this.numeroChiamate}`
+    }
+
+}
+
+
+
+const Call = new User(20, 0, 'Pippo', 'Biachi');
+console.log(Call);
+console.log(Call.ricarica(5));
+console.log(Call.chiamata(0))
+console.log(Call.chiama404());
+console.log(Call.getNumeroChiamta());
+
+
+const credito = document.getElementById('credito') as HTMLBodyElement;
+
+credito.innerText = `Il tuo credito è ${Call.credito} euro`;
+
+const minuti = document.getElementById('minuti') as HTMLBodyElement;
+
+minuti.innerText = `${Call.getNumeroChiamta()}`;
+
+const ricaricaInput = document.getElementById('inputRicarica') as HTMLInputElement;
+
+const btnRicarica = document.getElementById('btnRicarica') as HTMLBodyElement;
+
+btnRicarica.addEventListener('click', () =>{
+    let ammontareRicarica = parseInt(ricaricaInput.value);
+    credito.innerText = `Il tuo credito è ${Call.ricarica(ammontareRicarica)} euro`;
+
+})
+
+
+
+
+
+
+
+
