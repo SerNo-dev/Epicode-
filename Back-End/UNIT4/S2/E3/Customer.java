c3fc9b701e03763deba60c922f27f4a6131a93b1
Sergio <131872447+SerNo-dev@users.noreply.github.com>
2024-04-24 16:56:30 +0200

    Add files via upload

---
+++ Back-End/UNIT4/S2/E3/Customer.java
@@ -0,0 +1,44 @@
+public class Customer {
+    long id;
+    String name;
+    Integer tier;
+
+    public Customer(long id, String name, Integer tier) {
+        this.id = id;
+        this.name = name;
+        this.tier = tier;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getTier() {
+        return tier;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTier(Integer tier) {
+        this.tier = tier;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", tier=" + tier +
+                '}';
+    }
+}
