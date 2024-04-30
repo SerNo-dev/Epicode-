87b4f984ff3ce1e702ac8e3e76b57a0d163aad92
Sergio <131872447+SerNo-dev@users.noreply.github.com>
2024-04-30 09:24:43 +0200

    E1 S3

--- Back-End/UNIT4/S3/E1.sql
+++ Back-End/UNIT4/S3/E1.sql
@@ -1,7 +1,8 @@
-select * from public.clienti where nome='Mario';
+SELECT * FROM public.fatture
+ORDER BY numerofattura ASC select * from public.clienti where nome='Mario';
 
 select nome,cognome, count(*) from public.clienti 
-WHERE DATE_PART('year', annodinascita)::INTEGER = 1982
+WHERE Extract(year from annodinascita) = 1982
 group by nome,cognome;
 
 select count(*)
@@ -23,3 +24,28 @@ select extract(year from datafattura) anno, count(*) numero_di_fatture
 from public.fatture
 where iva = 20
 group by anno;
+
+select extract(year from datafattura) anno, count(*) numero_di_fatture, sum(importo) importo_totale
+from public.fatture
+where iva = 20
+group by anno;
+
+
+select extract(year from datafattura) anno, count(*) numero_fatture
+from public.fatture
+where tipologia='A'
+group by anno
+having count(*)>2;
+
+select c.regione_residenza,sum(f.importo)
+from public.fatture f inner join public.clienti  c
+on f.idcliente= c.numerocliente
+group by c.regione_residenza ;
+
+
+SELECT COUNT(DISTINCT c.numerocliente) 
+FROM public.clienti c 
+INNER JOIN public.fatture f ON c.numerocliente = f.idcliente 
+WHERE f.importo > 50 AND EXTRACT(year FROM c.annodinascita) = 1980;
+
+
